e5f06b47a78f4ec76d8c8483e6549f40734a9bd7
李杰 <lj706517903@163.com>
2017-07-18 15:55:39 +0800

    Update styles.js

--- build/Tab/styles.js
+++ build/Tab/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native'
-import { Platform } from 'react-native'
+import { Platform,Text } from 'react-native'
+import React from 'react'
 import Button from '../Touchable'
 import values from '../values'
 export const TabBody = styled.View`
@@ -11,11 +12,21 @@ export const TabBody = styled.View`
 export const TabButton = styled(Button)`
   width: ${props => props.tabWidth}
 `
-export const TabText = styled.Text`
-  color: ${props => props.color}
-  fontWeight: ${Platform.OS === 'ios' ? 500 : 400}
-  fontFamily: ${Platform.OS === 'android' ? 'sans-serif-medium' : 'System'}
-  fontSize: 14
-  textAlign: center
-  minWidth: 100%
-`
+
+export const TabText = (props)=>(
+    <Text
+        style = {[
+            {
+                color: props.color,
+                fontWeight: Platform.OS === 'ios' ? '500' : '400' ,
+                fontFamily: Platform.OS === 'android' ? 'sans-serif-medium' : 'System' ,
+                fontSize: 14,
+                textAlign: 'center',
+                minWidth: '100%',
+            },
+            props.textStyle
+        ]}
+    >
+        {props.children}
+    </Text>
+)
